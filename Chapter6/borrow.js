ee67bca31121855517971894ac37f86649833f97
George Kanellopoulos <gkanel97@hotmail.com>
2021-12-29 21:16:40 +0200

    Added Borrow a Method exercise

---
+++ Chapter6/borrow.js
@@ -0,0 +1,8 @@
+obj = {
+    hasOwnProperty: true,
+    hasOtherProperties: false
+}
+
+console.log(Object.prototype.hasOwnProperty.call(obj, 'hasOwnProperty'));        // true
+console.log(Object.prototype.hasOwnProperty.call(obj, 'hasOtherProperties'));    // true
+console.log(Object.prototype.hasOwnProperty.call(obj, 'hasNoProperties'));       // false
